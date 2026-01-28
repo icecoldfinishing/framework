313eca8c65cd812182baff1b25e9bec965068883
icecoldfinishing <rohyamboara@gmail.com>
2026-01-28 13:15:21 +0300

    ok

--- src/main/java/etu/sprint/annotation/Authorized.java
+++ src/main/java/etu/sprint/annotation/Authorized.java
@@ -5,13 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Annotation pour protéger les méthodes de controller selon les rôles.
- * Les rôles possibles sont : "admin", "anonym", "all"
- * - "admin" : nécessite un utilisateur authentifié avec le rôle admin
- * - "anonym" : accessible uniquement aux utilisateurs non authentifiés
- * - "all" : accessible à tous (pas de restriction)
- */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Authorized {
